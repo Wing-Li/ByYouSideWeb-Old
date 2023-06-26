f824cb91bb8447ace5e4f07a7aa08ea43c157728
Wing_Li <liyunliang@boo.chat>
2023-06-26 10:01:36 +0800

    添加版本判断

--- src/main/kotlin/com/lyl/byyouside/controller/api/VersionController.kt
+++ src/main/kotlin/com/lyl/byyouside/controller/api/VersionController.kt
@@ -1,18 +1,24 @@
 package com.lyl.byyouside.controller.api
 
+import com.lyl.byyouside.config.ContextHolder
 import com.lyl.byyouside.config.StatusCode
 import com.lyl.byyouside.controller.base.ApiBaseController
 import com.lyl.byyouside.model.base.BaseCallBack
+import com.lyl.byyouside.model.user.UserInfoRepository
 import com.lyl.byyouside.model.version.Version
 import com.lyl.byyouside.model.version.VersionRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.jvm.optionals.getOrNull
 
 
 @RestController
 class VersionController() : ApiBaseController() {
+
+    @Autowired
+    private lateinit var userInfoRepository: UserInfoRepository
 
     @Autowired
     private lateinit var versionRepository: VersionRepository
@@ -27,6 +33,15 @@ class VersionController() : ApiBaseController() {
         iosDownloadUrl: String,
         isForce: Boolean,
     ): BaseCallBack<Any> {
+        val userId = ContextHolder.userId
+        val userData = userInfoRepository.findById(userId).getOrNull()
+        userAuth(userData)?.let { return it }
+
+        if ("admin" != userData?.status) {
+            // 只有管理员才可以操作
+            return failCallBack(StatusCode.ERROR_10013, StatusCode.ERROR_10013_TEXT)
+        }
+
         val versionData = Version(
             title = title,
             description = description,
